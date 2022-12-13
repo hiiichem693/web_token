45c17eab2dbc23c9fea3679d5203d218adb8635f
Hichem <hichem.belala@lmdsio.fr>
2022-12-13 10:08:37 +0100

    Initial commit

---
+++ app/Models/Users.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class Users extends Authenticatable implements JWTSubject
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $table = "users";
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'id',
+        'name',
+        'email',
+        'password',
+        'remember_token',
+        'status',
+        'id_role',
+        'last_update',
+        'user_update',
+        'role',
+    ];
+
+    protected $hidden = [
+        'password' ];
+
+    public $timetamps = true;
+
+    public function getAuthPassword()
+    {
+        return $this->password;
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
